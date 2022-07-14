c9bae454a67bd928f80807232f8c816a1f797d3b
James P. Thomas <james@jamespthomas.com>
2022-07-14 02:51:04 -0400

    Lock fix

--- bridge/irc/irc.go
+++ bridge/irc/irc.go
@@ -160,10 +160,10 @@ func (b *Birc) Send(msg config.Message) (string, error) {
 	b.Log.Debugf("=> Receiving %#v", msg)
 
 	// we can be in between reconnects #385
-	if !b.i.IsConnected() {
-		b.Log.Error("Not connected to server, dropping message")
-		return "", nil
-	}
+//	if !b.i.IsConnected() {
+//		b.Log.Error("Not connected to server, dropping message")
+//		return "", nil
+//	}
 
 	// Execute a command
 	if strings.HasPrefix(msg.Text, "!") {
